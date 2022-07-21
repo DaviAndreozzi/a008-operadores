e97823e552468cbe4c1db7e73a0dc212cfa6e11c
Davi <Daviandreozzi7@gmail.com>
2022-07-20 21:46:35 -0300

    a008-operadores

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeroPar = prompt('Insira um numero Par!')
+
+const divisão = 2
+
+console.log('A divisão sera', numeroPar / divisão)
+// com numeros pares aparece resultados exatos
+// com numeros impares aparece resultados quebrados
